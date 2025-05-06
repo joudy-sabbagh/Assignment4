292e79ad3c4f16febe2ff1302248b100d653f644
joudy-sb <joudy.f.sabbagh@gmail.com>
2025-05-06 16:02:55 +0300

    Updated readme

--- Core/Entities/Attendee.cs
+++ Core/Entities/Attendee.cs
@@ -5,15 +5,8 @@ namespace MyMVCApp.Models
 {
     public class Attendee
     {
-        [Key]
         public int AttendeeId { get; set; }
-
-        [Required(ErrorMessage = "Attendee name is required.")]
-        [RegularExpression(@"^[A-Z].*$", ErrorMessage = "Attendee name must start with a capital letter.")]
         public string Name { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; } = string.Empty;
 
         // An Attendee can have many Tickets.
